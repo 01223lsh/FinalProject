6e26069da3a53610ffcacef701be2a42124ec08f
eunji <ohohdmswlgd@gmail.com>
2022-05-18 12:08:23 +0900

    머지 합병1

--- src/main/java/funding/dto/Order.java
+++ src/main/java/funding/dto/Order.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package funding.dto;
 
 import java.util.Date;
@@ -82,30 +82,3 @@ public class Order {
 	
 	
 }
-=======
-package funding.dto;
-
-public class Order {
-	
-	private int orderNo;
-	private int memberNo;
-	
-	@Override
-	public String toString() {
-		return "Order [orderNo=" + orderNo + ", memberNo=" + memberNo + "]";
-	}
-	public int getOrderNo() {
-		return orderNo;
-	}
-	public void setOrderNo(int orderNo) {
-		this.orderNo = orderNo;
-	}
-	public int getMemberNo() {
-		return memberNo;
-	}
-	public void setMemberNo(int memberNo) {
-		this.memberNo = memberNo;
-	}
-
-}
->>>>>>> origin/fund/login
